0710f356441abd3a20ec2e4b12ed8c6572da3207
roxreis <rodrigoassuncao12@gmail.com>
2019-10-15 20:07:33 -0300

    feito ate exercicio 12

--- loops.php
+++ loops.php
@@ -73,3 +73,63 @@ do {
         echo "Encontramos um $zeroADez[$a]!<br> <br>";
     }
 } while ($a < count($zeroADez));
+
+
+// exercicio 8
+
+foreach (range('a', 'n') as $index => $value) {
+
+    echo "Na posição $index está a letra $value <br>";
+} 
+
+// exercicio 9
+
+$mascote = ["Animal"=>"Cachorro", "Idade"=> 7, "Altura"=> "50cm", "Nome"=>"Rex"];
+foreach ($mascote as $key => $value) {
+    echo "<br> $key = $value <br>";
+}
+
+
+// exercicio 11
+
+$ceu = ["Itália"=>"Roma", "Luxemburgo"=>"Luxemburgo",
+"Bélgica"=> "Bruxelas", "Dinamarca"=>"Copenhagen",
+"Finlândia"=>"Helsinki", "França" => "Paris",
+"Eslováquia"=>"Bratislava", "Eslovênia"=>"Liubliana",
+"Alemanha" => "Berlim", "Grécia" => "Atenas",
+"Irlanda"=>"Dublin", "Holanda"=>"Amsterdã",
+"Portugal"=>"Lisboa", "Espanha"=>"Madri",
+"Suécia"=>"Estocolmo", "Reino Unido"=>"Londres",
+"Chipre"=>"Nicósia", "Lituânia"=>"Vilnius", "República
+Tcheca"=>"Praga", "Estônia"=>"Tallinn",
+"Hungria"=>"Budapeste","Letônia"=>"Riga", "Malta"=>"Valletta",
+"Áustria" => "Viena", "Polônia"=>"Varsóvia"];
+
+ksort($ceu);
+
+foreach ($ceu as $key => $value) {
+    echo "<br> A capital do país $key é $value<br>";
+}
+
+// exercicio 12
+
+$mundo = [
+    "Argentina" => ["Buenos Aires", "Córdoba", "Santa Fé"],
+    "Brasil" => ["Brasília", "Rio de Janeiro", "São Paulo"],
+    "Colômbia" => ["Cartagena", "Bogotá", "Barranquilla"],
+    "França" => ["Paris", "Nantes", "Lyon"],
+    "Itália" => ["Roma", "Milão", "Veneza"],
+    "Alemanha" => ["Munique", "Berlim", "Frankfurt"]
+    ];
+    
+    ksort($mundo); 
+
+    foreach ($mundo as $pais => $cidade) {
+        echo "<br> As cidades da $pais são: <br> ";
+        foreach ($cidade as $são => $cidade) {
+            echo $cidade."<br>";
+        }
+    }
+
+// exercicio 13
+
